afbc19990a4958477bf0e343ef11e9a8fd42e058
JM Hiver <jhiver@gmail.com>
2022-04-15 12:23:49 +0400

    fixed tests

--- tests/test_pause.py
+++ tests/test_pause.py
@@ -8,18 +8,18 @@ from brownie import reverts
 @pytest.fixture
 def mock():
     print("deploy mock token")
-    return accounts[0].deploy(EnFi20, "Mock", "FOO", 9, 1000*10**18)
+    return accounts[0].deploy(EnFi20, "Mock", "FOO", 9, 1000)
 
 
 def test_transfer(mock):
     assert(mock.address != ZERO_ADDRESS)
 
     # test transfer of tokens
-    assert(mock.balanceOf(accounts[0]) == 1000*10**18)
+    assert(mock.balanceOf(accounts[0]) == 1000*10**9)
     assert(mock.balanceOf(accounts[1]) == 0)
-    mock.transfer(accounts[1], 100*10**18, {"from": accounts[0]})
-    assert(mock.balanceOf(accounts[0]) == 900*10**18)
-    assert(mock.balanceOf(accounts[1]) == 100*10**18)
+    mock.transfer(accounts[1], 100*10**9, {"from": accounts[0]})
+    assert(mock.balanceOf(accounts[0]) == 900*10**9)
+    assert(mock.balanceOf(accounts[1]) == 100*10**9)
 
 
 def test_transferFrom(mock):
@@ -27,12 +27,12 @@ def test_transferFrom(mock):
 
     # test approve and transferFrom
     assert(mock.allowance(accounts[0], accounts[1]) == 0)
-    mock.approve(accounts[1], 100*10**18, {"from": accounts[0]})
-    assert(mock.allowance(accounts[0], accounts[1]) == 100*10**18)
-    mock.transferFrom(accounts[0], accounts[2], 100*10**18, {"from": accounts[1]})
-    assert(mock.balanceOf(accounts[0]) == 900*10**18)
+    mock.approve(accounts[1], 100*10**9, {"from": accounts[0]})
+    assert(mock.allowance(accounts[0], accounts[1]) == 100*10**9)
+    mock.transferFrom(accounts[0], accounts[2], 100*10**9, {"from": accounts[1]})
+    assert(mock.balanceOf(accounts[0]) == 900*10**9)
     assert(mock.balanceOf(accounts[1]) == 0)
-    assert(mock.balanceOf(accounts[2]) == 100*10**18)
+    assert(mock.balanceOf(accounts[2]) == 100*10**9)
 
 
 def test_pause(mock):
@@ -45,31 +45,31 @@ def test_pause(mock):
     assert(mock.paused({'from': accounts[1]}))
 
     # test transfer of tokens
-    assert(mock.balanceOf(accounts[0]) == 1000*10**18)
+    assert(mock.balanceOf(accounts[0]) == 1000*10**9)
     assert(mock.balanceOf(accounts[1]) == 0)
 
     # should work for contract owner...
-    mock.transfer(accounts[1], 100*10**18, {"from": accounts[0]})
-    assert(mock.balanceOf(accounts[0]) == 900*10**18)
-    assert(mock.balanceOf(accounts[1]) == 100*10**18)
+    mock.transfer(accounts[1], 100*10**9, {"from": accounts[0]})
+    assert(mock.balanceOf(accounts[0]) == 900*10**9)
+    assert(mock.balanceOf(accounts[1]) == 100*10**9)
 
     # should not work the other way around since the contract is paused
     with reverts():
-        mock.transfer(accounts[0], 100*10**18, {"from": accounts[1]})
-    assert(mock.balanceOf(accounts[0]) == 900*10**18)
-    assert(mock.balanceOf(accounts[1]) == 100*10**18)
+        mock.transfer(accounts[0], 100*10**9, {"from": accounts[1]})
+    assert(mock.balanceOf(accounts[0]) == 900*10**9)
+    assert(mock.balanceOf(accounts[1]) == 100*10**9)
 
     # try to unpause the contract from somewhere else, shouldn't work
     with reverts():
         mock.unpause({'from': accounts[1]})
-        mock.transfer(accounts[0], 100*10**18, {"from": accounts[1]})
-    assert(mock.balanceOf(accounts[0]) == 900*10**18)
-    assert(mock.balanceOf(accounts[1]) == 100*10**18)
+        mock.transfer(accounts[0], 100*10**9, {"from": accounts[1]})
+    assert(mock.balanceOf(accounts[0]) == 900*10**9)
+    assert(mock.balanceOf(accounts[1]) == 100*10**9)
 
     # unpause from owner, should be fine
     mock.unpause({'from': accounts[0]})
-    mock.transfer(accounts[0], 100*10**18, {"from": accounts[1]})
-    assert(mock.balanceOf(accounts[0]) == 1000*10**18)
+    mock.transfer(accounts[0], 100*10**9, {"from": accounts[1]})
+    assert(mock.balanceOf(accounts[0]) == 1000*10**9)
     assert(mock.balanceOf(accounts[1]) == 0)
 
 
@@ -83,29 +83,29 @@ def test_pause_address(mock):
     assert(not mock.paused(accounts[2]))
 
     # test transfer of tokens
-    assert(mock.balanceOf(accounts[0]) == 1000*10**18)
+    assert(mock.balanceOf(accounts[0]) == 1000*10**9)
     assert(mock.balanceOf(accounts[1]) == 0)
 
     # should work for contract owner...
-    mock.transfer(accounts[1], 100*10**18, {"from": accounts[0]})
-    assert(mock.balanceOf(accounts[0]) == 900*10**18)
-    assert(mock.balanceOf(accounts[1]) == 100*10**18)
+    mock.transfer(accounts[1], 100*10**9, {"from": accounts[0]})
+    assert(mock.balanceOf(accounts[0]) == 900*10**9)
+    assert(mock.balanceOf(accounts[1]) == 100*10**9)
 
     # should not work the other way around since the contract is paused
     with reverts():
-        mock.transfer(accounts[0], 100*10**18, {"from": accounts[1]})
-    assert(mock.balanceOf(accounts[0]) == 900*10**18)
-    assert(mock.balanceOf(accounts[1]) == 100*10**18)
+        mock.transfer(accounts[0], 100*10**9, {"from": accounts[1]})
+    assert(mock.balanceOf(accounts[0]) == 900*10**9)
+    assert(mock.balanceOf(accounts[1]) == 100*10**9)
 
     # try to unpause the contract from somewhere else, shouldn't work
     with reverts():
         mock.unpause(accounts[1], {'from': accounts[1]})
-        mock.transfer(accounts[0], 100*10**18, {"from": accounts[1]})
-    assert(mock.balanceOf(accounts[0]) == 900*10**18)
-    assert(mock.balanceOf(accounts[1]) == 100*10**18)
+        mock.transfer(accounts[0], 100*10**9, {"from": accounts[1]})
+    assert(mock.balanceOf(accounts[0]) == 900*10**9)
+    assert(mock.balanceOf(accounts[1]) == 100*10**9)
 
     # unpause from owner, should be fine
     mock.unpause(accounts[1], {'from': accounts[0]})
-    mock.transfer(accounts[0], 100*10**18, {"from": accounts[1]})
-    assert(mock.balanceOf(accounts[0]) == 1000*10**18)
+    mock.transfer(accounts[0], 100*10**9, {"from": accounts[1]})
+    assert(mock.balanceOf(accounts[0]) == 1000*10**9)
     assert(mock.balanceOf(accounts[1]) == 0)
